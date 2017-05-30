c47b731dd5766651e734d41742b0b8a80cb2f452
To Ka <toka@helix.ge>
2017-05-30 22:52:59 +0400

    added production processing and versioning
    Added gulp-if and yards processing
    
    —prod or —production will build production build.
    version number will be added to sprite image url in css

--- gulpfile.js
+++ gulpfile.js
@@ -9,22 +9,32 @@ var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 var webserver = require('gulp-webserver');
+var argv = require('yargs').argv;
+var gulpif = require('gulp-if');
 
+var settings = {
+	build: {
+		prod: argv.production || argv.prod,
+		version: parseInt(Math.random() * 10000000) + "-" + parseInt(Math.random() * 10000000)
+	}
+}
 
-gulp.task('styles', ['sprite'], function() {
-    gulp.src('./m/_scss/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-		.pipe(sourcemaps.init())
+gulp.task('styles', ['sprite'], function () {
+	gulp.src([
+		'./m/_scss/**/*.scss'
+	])
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulpif(!settings.build.prod, sourcemaps.init()))
 		.pipe(cleanCSS())
-		.pipe(sourcemaps.write())
-        .pipe(gulp.dest('./m/css'));
+		.pipe(gulpif(!settings.build.prod, sourcemaps.write()))
+		.pipe(gulp.dest('./m/css'));
 });
 
-gulp.task('html', function() {
-    gulp.src('./m/_templates/*.html')
-        .pipe(nunjucks({
-            searchPaths: ['./m/_templates'],
-            tags: {
+gulp.task('html', function () {
+	gulp.src('./m/_templates/*.html')
+		.pipe(nunjucks({
+			searchPaths: ['./m/_templates'],
+			tags: {
 				blockStart: '<%',
 				blockEnd: '%>',
 				variableStart: '<$',
@@ -32,87 +42,87 @@ gulp.task('html', function() {
 				commentStart: '<#',
 				commentEnd: '#>'
 			}
-        }))
-        .pipe(gulp.dest('./'));
+		}))
+		.pipe(gulp.dest('./'));
 });
 
- gulp.task('js-pre', function() {
-    gulp.src([
-			'./m/_vendor/jquery/dist/jquery.min.js',
-			'./m/_vendor/bootstrap-sass/assets/javascripts/bootstrap.min.js',
-			'./m/_vendor/helix.jquery-equalheights/src/jquery.equalheights.js',
-			'./m/_vendor/underscore/underscore-min.js',
-			'./m/js/app/pre/**/*.js'
-			])
-	    .pipe(sourcemaps.init())
+gulp.task('js-pre', function () {
+	gulp.src([
+		'./m/_vendor/jquery/dist/jquery.min.js',
+		'./m/_vendor/bootstrap-sass/assets/javascripts/bootstrap.min.js',
+		'./m/_vendor/helix.jquery-equalheights/src/jquery.equalheights.js',
+		'./m/_vendor/underscore/underscore-min.js',
+		'./m/js/app/pre/**/*.js'
+	])
+		.pipe(gulpif(!settings.build.prod, sourcemaps.init()))
 		.pipe(concat('pre.min.js'))
-		.pipe(uglify())
-		.pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('./m/js/'));
+		.pipe(gulpif(!settings.build.prod, uglify()))
+		.pipe(gulpif(!settings.build.prod, sourcemaps.write('./')))
+		.pipe(gulp.dest('./m/js/'));
 });
 
-gulp.task('js-post', function() {
-    gulp.src([
-			'./m/js/app/post/**/*.js',
-			])
-	    .pipe(sourcemaps.init())
+gulp.task('js-post', function () {
+	gulp.src([
+		'./m/js/app/post/**/*.js',
+	])
+		.pipe(gulpif(!settings.build.prod, sourcemaps.init()))
 		.pipe(concat('post.min.js'))
-		.pipe(uglify())
-		.pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('./m/js/'));
+		.pipe(gulpif(!settings.build.prod, uglify()))
+		.pipe(gulpif(!settings.build.prod, sourcemaps.write('./')))
+		.pipe(gulp.dest('./m/js/'));
 });
 
-gulp.task('js', ['js-pre', 'js-post'], function() {
-	
+gulp.task('js', ['js-pre', 'js-post'], function () {
+
 });
 
-gulp.task('sprite', function() {
-    var spriteData = gulp.src('./m/i/_spritesource/**/*.png')
-        .pipe(spritesmith({
+gulp.task('sprite', function () {
+	var spriteData = gulp.src('./m/i/_spritesource/**/*.png')
+		.pipe(spritesmith({
 			padding: 15,
-            imgName: 'sprite.png',
-            imgPath: '../i/sprite.png',
-            cssName: '_sprites.scss',
+			imgName: 'sprite.png',
+			imgPath: '../i/sprite.png?' + settings.build.version,
+			cssName: '_sprites.scss',
 			retinaSrcFilter: ['./m/i/_spritesource/**/*@2x.png'],
 			retinaImgName: 'sprite@2x.png',
-			retinaImgPath: '../i/sprite@2x.png',
-        }));
+			retinaImgPath: '../i/sprite@2x.png?' + settings.build.version,
+		}));
 
-    var imgStream = spriteData.img
-        .pipe(gulp.dest('./m/i/'));
+	var imgStream = spriteData.img
+		.pipe(gulp.dest('./m/i/'));
 
-    var cssStream = spriteData.css
-        .pipe(gulp.dest('./m/_scss/'));
+	var cssStream = spriteData.css
+		.pipe(gulp.dest('./m/_scss/'));
 
-    return merge(imgStream, cssStream);
+	return merge(imgStream, cssStream);
 });
 
-gulp.task('webserver', ['default'], function(){
-  gulp.src('./')
-    .pipe(webserver({
-		livereload: {
-			enable: true,
-			filter: function(fileName) {
-				if (fileName.match(/.map$/)) {
-					return false;
-				} else {
-					return true;
+gulp.task('webserver', ['default'], function () {
+	gulp.src('./')
+		.pipe(webserver({
+			livereload: {
+				enable: true,
+				filter: function (fileName) {
+					if (fileName.match(/.map$/)) {
+						return false;
+					} else {
+						return true;
+					}
 				}
-			}
-		},
-		directoryListing: true,
-		open: true
-    }));     
+			},
+			directoryListing: true,
+			open: true
+		}));
 });
 
-gulp.task('process', ['styles', 'html', 'js'], function() {
-	
+gulp.task('process', ['styles', 'html', 'js'], function () {
+
 });
 
-gulp.task('default', ['process'], function() {
-    gulp.watch('./m/_scss/**/*', ['styles']);
-    gulp.watch('./m/_templates/**/*', ['html']);
-    gulp.watch('./m/i/_spritesource/**/*', ['sprite']);
+gulp.task('default', ['process'], function () {
+	gulp.watch('./m/_scss/**/*', ['styles']);
+	gulp.watch('./m/_templates/**/*', ['html']);
+	gulp.watch('./m/i/_spritesource/**/*', ['sprite']);
 	gulp.watch('./m/_vendor/**/*', ['js']);
 	gulp.watch('./m/js/app/**/*', ['js']);
 });
